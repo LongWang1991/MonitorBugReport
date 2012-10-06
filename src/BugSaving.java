bc4cc3f1f10d967c9705224f365bb54874937e8a
Wang Long <Long.Wang1991@gmail.com>
2012-10-07 02:38:06 +0800

    Save Fixed Date, Platform, Signature to TXT File

--- src/BugSaving.java
+++ src/BugSaving.java
@@ -20,8 +20,15 @@ import java.util.TimerTask;
 
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
+import org.htmlparser.filters.AndFilter;
 import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.HasChildFilter;
+import org.htmlparser.filters.HasParentFilter;
+import org.htmlparser.filters.NodeClassFilter;
 import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.tags.TableColumn;
+import org.htmlparser.tags.TableRow;
+import org.htmlparser.tags.TableTag;
 import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
@@ -34,30 +41,14 @@ public class BugSaving {
     private static String ENCODE = "GBK";
     
     ArrayList allBugID= new ArrayList();
-//
-//    public static String openFile( String szFileName ) {//
-//        try {
-//            BufferedReader bis = new BufferedReader(new InputStreamReader(new FileInputStream( new File(szFileName)),    ENCODE) );
-//            String szContent="";
-//            String szTemp;
-//            
-//            while ( (szTemp = bis.readLine()) != null) {
-//                szContent+=szTemp+"\n";
-//            }
-//            bis.close();
-//            return szContent;
-//        }
-//        catch( Exception e ) {
-//            return "";
-//        }
-//    }
-    
+
     public ArrayList getBugID(BugSaving bs){//get all bug id from web site 
     	ArrayList bugID = new ArrayList();
     	try{          
             //Parser parser = new Parser((HttpURLConnection) (new URL("https://bugzilla.mozilla.org/buglist.cgi?resolution=---;resolution=DUPLICATE;chfieldto=Now;query_format=advanced;chfieldfrom="+bs.getDate())).openConnection());       
-            Parser parser = new Parser((HttpURLConnection) (new URL("https://bugzilla.mozilla.org/buglist.cgi?field0-0-0=cf_crash_signature;resolution=FIXED;chfieldto=Now;query_format=advanced;type0-0-0=isnotempty;chfieldfrom="+bs.getDate())).openConnection());       
-                        
+            //Parser parser = new Parser((HttpURLConnection) (new URL("https://bugzilla.mozilla.org/buglist.cgi?order=Importance;field0-0-0=cf_crash_signature;resolution=FIXED;chfieldto=Now;query_format=advanced;chfield=cf_crash_signature;chfieldfrom="+bs.getDate()+";type0-0-0=isnotempty")).openConnection());       
+            Parser parser = new Parser((HttpURLConnection) (new URL("https://bugzilla.mozilla.org/buglist.cgi?order=Importance;field0-0-0=cf_crash_signature;resolution=FIXED;chfieldto=Now;query_format=advanced;chfield=cf_crash_signature;chfieldfrom="+bs.getDate()+";type0-0-0=isnotempty")).openConnection());       
+            
             
             
             NodeFilter filter = new HasAttributeFilter( "name", "id");
@@ -87,16 +78,13 @@ public class BugSaving {
                        		 isEqual = true;
                        	 }
                        	  
-                         }
-                  	  if(!isEqual){
-                      	  bugID.add(value);
-                      	  
-                    }
+                   }
+                   if(!isEqual){
+                      	  bugID.add(value);     	  
+                   }
+                                       
+                   }
                     
-                    
-                    }
-                    
-
                 }
                
             }
@@ -161,11 +149,11 @@ public class BugSaving {
 
     }
 
-    public void save(String name,String bs)throws IOException{//Create html file and save it.
+    public void saveAsHTML(String name,String bs)throws IOException{//Create html file and save it.
     	 
-    	 String filepath2="E:\\" + name + ".html";
+    	 String filepath="E:\\" + name + ".html";
     	 
-    	 FileWriter fw=new FileWriter(filepath2);
+    	 FileWriter fw=new FileWriter(filepath);
     	 BufferedWriter bw=new BufferedWriter(fw);
     	 PrintWriter pw=new PrintWriter(bw);
     	 
@@ -178,6 +166,23 @@ public class BugSaving {
     	 
     	 
     	 } 
+    public void saveAsTXT(String name,String bs)throws IOException{//Create txt file and save it.
+   	 
+   	 String filepath="E:\\" + name + ".txt";
+   	 
+   	 FileWriter fw=new FileWriter(filepath);
+   	 BufferedWriter bw=new BufferedWriter(fw);
+   	 PrintWriter pw=new PrintWriter(bw);
+   	 
+   	 
+   	 pw.print(bs);
+   	 
+   	 
+   	 bw.close();
+   	 fw.close();
+   	 
+   	 
+   	 } 
 
 public void saveHtml(String id,BugSaving bs){
     	
@@ -195,7 +200,7 @@ public void saveHtml(String id,BugSaving bs){
                         if(!textnode.toPlainTextString().equals("")){
                         	
                         	String htmlString = bs.GetURLstr(url);
-                        	bs.save(id, htmlString);
+                        	bs.saveAsHTML(id, htmlString);
                         	
                         }
                     }
@@ -210,6 +215,28 @@ public void saveHtml(String id,BugSaving bs){
 		}
     	//return htmlContent;	
     }
+
+public void saveTXT(String id,BugSaving bs){ //get all the string to save to txt file
+	
+	String fixedDate = bs.getFixedDate(id);
+	String platform = bs.getPlatform(id);
+	String[] crashSignatureArray = bs.getCrashSignature(id);
+	
+	String signature="";
+	
+	System.out.println("platform:"+platform);
+	for(int i=0;i<crashSignatureArray.length;i++){
+		signature+="Signature:"+crashSignatureArray[i].replace("\n", "")+"\r\n";
+	}
+	String fdAndPf = "FixedDate:"+fixedDate+"\r\n"+"Platform:"+platform+"\r\n";
+	String txtFile = fdAndPf + signature;
+	try {
+		bs.saveAsTXT(id, txtFile);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
     
    public String getDate(){
 	   
@@ -232,12 +259,127 @@ public void saveHtml(String id,BugSaving bs){
 		   if(!isEqual){
 			   allBugID.add(id.get(i)); 
 			   bs.saveHtml((String)id.get(i), bs);
+			   bs.saveTXT((String)id.get(i), bs);
+			   
 		   }
 		   
 	   }
 	   
    }
     
+   public String getPlatform(String id){// get Platform information
+	   String platform = null;
+   	try {
+   		
+   		String url = new String("https://bugzilla.mozilla.org/show_bug.cgi?id="+id);
+   		
+   			Parser parser = new Parser((HttpURLConnection) (new URL(url).openConnection())); 
+            NodeFilter filter = new HasAttributeFilter("class","field_value");
+       
+            NodeList nodes = parser.extractAllNodesThatMatch(filter);
+               
+               if(nodes!=null) {
+                   for (int k = 0; k < nodes.size(); k++) {
+                       Node textnode = (Node) nodes.elementAt(k);
+                       Node parent = textnode.getParent(); 
+                       
+                       String parentText = parent.toPlainTextString().replace(" ", "").replace("\n", "");                         
+                       String[] array = parentText.split(":");
+                       
+                       for(int i=0;i<array.length;i++){
+                    	   if(array[i].equals("Platform")){
+                    		   platform = array[i+1];
+                    	   }
+                       }
+                   }
+               }   
+   		
+   		
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Getting Platform is wrong");
+			e.printStackTrace();
+		}
+   	//return htmlContent;	
+   	return platform;
+   }
+   
+public String[] getCrashSignature(String id){//get CrashSignature information
+   	String[] crashSignatureArray = null;
+   	try {
+   		
+   		String url = new String("https://bugzilla.mozilla.org/show_bug.cgi?id="+id);
+   			Parser parser = new Parser((HttpURLConnection) (new URL(url).openConnection()));
+   			NodeFilter filter = new HasAttributeFilter( "class", "uneditable_textarea");
+               NodeList nodes = parser.extractAllNodesThatMatch(filter);
+               
+               if(nodes!=null) {
+                   for (int k = 0; k < nodes.size(); k++) {
+                       Node textnode = (Node) nodes.elementAt(k);
+                      
+                       if(!textnode.toPlainTextString().equals("")){
+                       	
+                       	String crashSignatureString = textnode.toPlainTextString();
+                       	crashSignatureArray = crashSignatureString.split("\n");
+
+                       }
+                   }
+               }     
+   		
+   		
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Getting CrashSignature is wrong");
+			e.printStackTrace();
+		}
+   	return crashSignatureArray;
+   }
+   
+public String getFixedDate(String id){//get FixedDate information
+	   String fixedDate = null;
+	try {
+		
+		String url = new String("https://bugzilla.mozilla.org/show_activity.cgi?id="+id);
+		
+			Parser parser = new Parser((HttpURLConnection) (new URL(url).openConnection())); 
+			NodeFilter tableFilter = new NodeClassFilter(TableTag.class);
+    
+			NodeList tableList = parser.extractAllNodesThatMatch(tableFilter);
+            
+            if(tableList!=null) {
+                for (int i = 0; i < tableList.size(); i++) {
+                	 TableTag table = (TableTag) tableList.elementAt(i);  //get all the table
+                     TableRow[] rows = table.getRows();  //put all the rows to an array
+                     for (int r=0; r<rows.length; r++) {
+                         TableRow tr = rows[r];
+                         TableColumn[] td = tr.getColumns();//get all the columns of one row
+                         
+                         for (int c=0; c<td.length; c++) {
+                               if(td[c].toPlainTextString().trim().equals("RESOLVED")){                         	
+                               fixedDate=td[1].toPlainTextString().trim();                          
+                            }
+                         }
+                        
+                     }
+                    
+                }
+                
+            }   
+		
+		
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Getting FixedDate is wrong");
+			e.printStackTrace();
+		}
+	
+		
+	return fixedDate;
+}
+
    public static void main(String[] args) {
         
 	   
@@ -254,14 +396,10 @@ public void saveHtml(String id,BugSaving bs){
 		   // TODO Auto-generated catch block
 		   e.printStackTrace();
 		   }
-
-	 
+ 
        
     }
    
-//   public void gg(){
-//	   System.out.println("aaaa");
-//   }
    
    static class MyTask extends TimerTask{
 	   
@@ -269,8 +407,7 @@ public void saveHtml(String id,BugSaving bs){
 		public void run() {
 			System.out.println("=========Every 10 minutes==========");
 			BugSaving bs = new BugSaving();
-			bs.doSave();
-			//bs.gg();
+			bs.doSave();	
 		}
 		   
 	   }
